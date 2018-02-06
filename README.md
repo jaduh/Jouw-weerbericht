16aa86e251114300e1b26fce2bd1ab286f8efbea
jaduh <jadeyorks@outlook.com>
2018-02-06 21:15:59 +0100

    meer functionaliteit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Jouw-weerbericht
+Jouw Weerbericht
+
+Deze app, zo simpel als hij is, leerde mij veel nieuws. 
+
+
+Het idee was om een webapplicatie te maken die aan de gebruiker vraagt of hij zijn locatie wil delen. Bij toestemming bezorgt de applicatie hem het actuele weerbericht van de locatie waarin de gebruiker zich op dat moment bevint.
+
+Extra functionaliteit is dat de gebruiker kan kiezen voor de temperatuur in celcius of in fahrenheit en dat, afhankelijk van het weertype, steeds een ander weersymbooltje wordt weergegeven.
+
+
+------- Geolocation.getCurrentPosition() method -------
+
+Om toegang te vragen tot de locatie van de gebruiker leerde ik de 'Geolocation.getCurrentPosition() method' te gebruiken.
+
+------ Api's ------
+
+Om de gebruiker vervolgens toegang te geven tot weerdata van zijn locatie, maakte ik gebruik van de API van openweathermap.org. Belangrijk voor mij was om het verzoek tot json data in vanilla js te doen en geen gebruik te maken van jquery of andere libary's. Ik gebruikte hiervoor 'Fetch'. Daarmee leerde ik ineens promises te gebruiken: 
+
+fetch(apiRequest).then(function(res) {
+                return res.json();
+            }).then(function(json) { 
+            ////code
+            } 
+
+
--- weather-app.js
+++ weather-app.js
@@ -1,5 +1,6 @@
 var weerTypes = [
     'Clear Sky',
+    'Clear',
     'Clouds',
     'Few Clouds',
     'Scattered Clouds',
@@ -11,13 +12,14 @@ var weerTypes = [
     'Mist'
 ];
 
-var weerSymbolen = ['https://image.flaticon.com/icons/svg/136/136832.svg', 'https://image.flaticon.com/icons/svg/131/131043.svg',
-'https://image.flaticon.com/icons/svg/131/131043.svg', 'https://image.flaticon.com/icons/svg/131/131077.svg', 'https://image.flaticon.com/icons/svg/131/131043.svg', 'https://image.flaticon.com/icons/svg/414/414923.svg', 'https://image.flaticon.com/icons/svg/69/69404.svg', 'https://image.flaticon.com/icons/svg/178/178374.svg', 'https://image.flaticon.com/icons/svg/653/653600.svg', 'https://image.flaticon.com/icons/svg/134/134120.svg'
+var weerSymbolen = ['https://image.flaticon.com/icons/svg/136/136832.svg',
+    'https://image.flaticon.com/icons/svg/136/136832.svg', 'https://image.flaticon.com/icons/svg/131/131043.svg',
+    'https://image.flaticon.com/icons/svg/131/131043.svg', 'https://image.flaticon.com/icons/svg/131/131077.svg', 'https://image.flaticon.com/icons/svg/131/131043.svg', 'https://image.flaticon.com/icons/svg/414/414923.svg', 'https://image.flaticon.com/icons/svg/69/69404.svg', 'https://image.flaticon.com/icons/svg/178/178374.svg', 'https://image.flaticon.com/icons/svg/653/653600.svg', 'https://image.flaticon.com/icons/svg/134/134120.svg'
 ];
 
 
 
-//API KEY: 8d8ae780c690c3afc002ee7aef0ffece
+
 
 var x = document.getElementById("naamStad");
 var y = document.getElementById("temperatuur");
@@ -25,7 +27,8 @@ var z = document.getElementById("weer");
 var a = document.getElementById("toestemming-tekst");
 var b = document.getElementById("weerSymbool");
 
-var celcius;
+var celcius = document.querySelector("#celcius");
+var fahrenheit = document.querySelector("#fahrenheit");
 
 
 
@@ -51,26 +54,31 @@ var currentPosition = (function() {
                 return res.json();
             }).then(function(json) {
                 x.innerHTML = json.name;
-                y.innerHTML = Math.floor(json.main.temp) + '';
+                y.innerHTML = Math.round(json.main.temp - 273.15) + "°";
                 z.innerHTML = json.weather[0].main;
-//                a.style.display = "none";
+               
+                fahrenheit.addEventListener('click', function(){
+                    y.innerHTML = Math.round(json.main.temp * (9/5) - 459.67) + "°";
+                })
+                
+                celcius.addEventListener('click', function(){
+                    y.innerHTML = Math.round(json.main.temp - 273.15) + "°";
+                })
 
-                console.log(json);
-                console.log(json.name);
-       
-//weersymbolen
+
+               
+
+                //weersymbolen
                 for (var i = 0; i < weerTypes.length; i++) {
 
-                    weerTypes[i] == json.weather[0].main ? b.src = weerSymbolen[i] : b.innerHTML = "fout";
+                    weerTypes[i] == json.weather[0].main ? b.src = weerSymbolen[i] : b.innerHTML = "Sorry, het weerbericht van jouw locatie is nu even niet beschikbaar";
 
                 }
             });
 
 
         })
-
-    } else {
-        console.log("hij doet het niet");
-    }
+    } 
 
 })();
+
